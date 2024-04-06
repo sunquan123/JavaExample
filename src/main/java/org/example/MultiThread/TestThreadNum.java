0fe24d3d80263a76e1462a6b74031457efe59713
sunquan <sunquan@sunline.cn>
2024-04-06 17:34:01 +0800

    增加java多线程示例

---
+++ src/main/java/org/example/MultiThread/TestThreadNum.java
@@ -0,0 +1,22 @@
+package org.example.MultiThread;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
+public class TestThreadNum {
+  public static void main(String[] args) {
+    ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+    ThreadInfo[] threadInfos = threadMXBean.dumpAllThreads(false, false);
+    for (ThreadInfo threadInfo : threadInfos) {
+      System.out.println(
+          "["
+              + threadInfo.getThreadId()
+              + "]  ThreadName:["
+              + threadInfo.getThreadName()
+              + "]  ThreadState:["
+              + threadInfo.getThreadState()
+              + "]");
+    }
+  }
+}
